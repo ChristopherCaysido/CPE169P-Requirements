4a9de7084d801b592925ad1ff302eaa30d138934
ChristopherCaysido <42668379+ChristopherCaysido@users.noreply.github.com>
2021-04-14 11:42:05 +0800

    new layer

--- Mod3_Project/neuralNetworkScratch.py
+++ Mod3_Project/neuralNetworkScratch.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-inputs = [1,2,3,4]
-weights = [0.5,-0.8,0.10,0.7]
-bias = 2
-output = (inputs[0]*weights[0]+
-        inputs[1]*weights[1]+
-        inputs[2]*weights[2]+
-        inputs[3]*weights[3]+
-        bias)
-print(output)
+inputs = [1,2,3,2.5]
+weights = [[0.2,0.8,-0.5,1],[0.5,-0.91,0.26,-0.5],[-0.26,-0.27,0.17,0.87]]
+biases = [2,3,0.5]
+layers_output = []
+for neuron_weights, neuron_biases in zip(weights,biases):
+    print(neuron_weights)
+    neuron_output = 0
+    for n_input, weight in zip(inputs,neuron_weights):
+        neuron_output += n_input*weight 
+    neuron_output += neuron_biases
+    layers_output.append(neuron_output)
+print(layers_output)
