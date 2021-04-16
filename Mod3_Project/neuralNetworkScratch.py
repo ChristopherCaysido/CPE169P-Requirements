0c5e2d342b9f80fd1be48bd3d59ba3df2c8819a8
ChristopherCaysido <42668379+ChristopherCaysido@users.noreply.github.com>
2021-04-16 11:48:02 +0800

    numpy neuron

--- Mod3_Project/neuralNetworkScratch.py
+++ Mod3_Project/neuralNetworkScratch.py
@@ -1,14 +1,7 @@
 import numpy as np
 
-inputs = [1,2,3,2.5]
-weights = [[0.2,0.8,-0.5,1],[0.5,-0.91,0.26,-0.5],[-0.26,-0.27,0.17,0.87]]
-biases = [2,3,0.5]
-layers_output = []
-for neuron_weights, neuron_biases in zip(weights,biases):
-    print(neuron_weights)
-    neuron_output = 0
-    for n_input, weight in zip(inputs,neuron_weights):
-        neuron_output += n_input*weight 
-    neuron_output += neuron_biases
-    layers_output.append(neuron_output)
-print(layers_output)
+inputs = [ 1.0 , 2.0 , 3.0 , 2.5 ]
+weights = [ 0.2 , 0.8 , - 0.5 , 1.0 ]
+bias = 2.0
+outputs = np.dot(weights, inputs) + bias
+print (outputs)
